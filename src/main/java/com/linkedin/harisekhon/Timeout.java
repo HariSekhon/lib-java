2a9341f4548f14b1c9293da9bcc1718b32fbb6c2
Hari Sekhon <harisekhon@gmail.com>
2016-06-15 15:07:02 +0100

    added src/main/java/com/linkedin/harisekhon/Timeout.java

---
+++ src/main/java/com/linkedin/harisekhon/Timeout.java
@@ -0,0 +1,54 @@
+//
+//  Author: Hari Sekhon
+//  Date: 2016-06-14 12:06:17 +0100 (Tue, 14 Jun 2016)
+//
+//  https://github.com/harisekhon/lib-java
+//
+//  License: see accompanying Hari Sekhon LICENSE file
+//
+//  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback
+//  to help improve or steer this or other code I publish
+//
+//  https://www.linkedin.com/in/harisekhon
+//
+
+
+package com.linkedin.harisekhon;
+
+import static com.linkedin.harisekhon.Utils.*;
+
+import java.util.concurrent.TimeoutException;
+
+public class Timeout implements Runnable {
+
+    private int secs;
+
+    public Timeout(int secs) {
+        this.secs = secs;
+    }
+
+    public void run() {
+        timeout(secs);
+    }
+
+    private void timeout(long secs) {
+        try {
+            Thread.sleep(1000 * secs);
+        } catch (InterruptedException e) {
+            // pass
+        }
+        // can't throw checked exceptions from Runnable :(
+//        throw new TimeoutException();
+//        throw new RuntimeException(String.format("self timed out after %s sec%s", secs, plural(secs)));
+        quit("UNKNOWN", String.format("self timed out after %s sec%s", secs, plural(secs)));
+    }
+
+    public static void main(String[] args) {
+        int secs = 10;
+        if (args.length > 0) {
+            secs = Integer.valueOf(args[0]);
+        }
+        (new Thread(new Timeout(secs))).start();
+    }
+
+}
